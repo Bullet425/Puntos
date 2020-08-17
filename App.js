ac8a40d55affa87159d5218dc6a2f394ffbc8b3a
Bullet425 <javier_mtze@hotmail.com>
2020-08-17 12:31:02 -0500

    First Commit App Mapa Pointers

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Map, Modal, Panel, Input } from "./components";
 
 export default function App() {
+  const [puntos, setPuntos] = useState([]);
+  const [nombre, setNombre] = useState("");
+  const [puntoTemp, setPuntoTemp] = useState({});
+  const [visibility, setVisibility] = useState(false);
+  const handleLongPress = ({ nativeEvent }) => {
+    //const newPuntos = [...puntos, { coordinate: nativeEvent.coordinate }]; // puntos.concat({ coordinate: nativeEvent.coordinate });
+    setPuntoTemp(nativeEvent.coordinate);
+    setVisibility(true);
+  };
+
+  const handleChangeText = (text) => {
+    setNombre(text);
+  };
+
+  const handleSubmit = () => {
+    const newPunto = {
+      coordinate: puntoTemp,
+      name: nombre,
+    };
+    setPuntos([...puntos, newPunto]);
+    setVisibility(false);
+    setNombre("");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Map onLongPress={handleLongPress} />
+      <Panel />
+      <Modal visibility={visibility}>
+        <Input
+          title="Nombre"
+          placeholder="Nombre del Punto"
+          onChangeText={handleChangeText}
+        />
+        <Button title="Guardar" onPress={handleSubmit} />
+      </Modal>
     </View>
   );
 }
@@ -14,8 +46,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start",
   },
 });
---
+++ components/Map.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MapView from "react-native-maps";
+import { StyleSheet, Dimensions } from "react-native";
+
+export default ({ onLongPress }) => {
+  return <MapView style={styles.map} onLongPress={onLongPress} />;
+};
+
+const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height - 150,
+  },
+});
